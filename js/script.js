e0bacd554051ed5f070df8c37846eadb29f7ea67
Hailie Jade Juliano <hjuliano@hawk.iit.edu>
2025-03-06 12:08:57 -0600

    finished mobile design

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
-var slideIndex = 0;
-carousel();
+var slideIndex = 1;
+showDivs(slideIndex);
 
-function carousel() {
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
   var i;
-  var x = document.getElementsByClassName("mySlides");
+  var x = document.getElementsByClassName("Slides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
   x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 8000);
 }
